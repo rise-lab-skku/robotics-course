f4f034fe9d31c28ab70f45776f198a6e60971559
Hong-ryul Jung <jung.hr.1206@gmail.com>
2022-01-16 04:08:27 +0900

    wip

--- kinematics_demo/src/ws_drawing.cpp
+++ kinematics_demo/src/ws_drawing.cpp
@@ -1,6 +1,6 @@
 /**
  * Boundary searching algorithm for the workspace of a robot.
- * Idea1: Octree, Quadtree
+ * Idea1: Octree
  * Idea2: Marching squares
  */
 #include <ros/ros.h>
@@ -12,6 +12,34 @@
 
 namespace rvt = rviz_visual_tools;
 
+void calcFK(
+    const std::vector<double>& joint_values,
+    const robot_state::RobotStatePtr& kinematic_state,
+    const robot_model::JointModelGroup* joint_model_group,
+    const std::string& eef_link,
+    geometry_msgs::Pose& pose)
+{
+    kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
+    Eigen::Affine3d eef_transformation = kinematic_state->getGlobalLinkTransform(eef_link);
+    pose = Eigen::toMsg(eef_transformation);
+}
+
+bool calcIK(
+    const geometry_msgs::Pose& eef_pose,
+    const robot_state::RobotStatePtr& kinematic_state,
+    const robot_model::JointModelGroup* joint_model_group,
+    std::vector<double>& joint_values)
+{
+    const unsigned int attempts = 10;
+    const double timeout = 0.1;
+    bool found_ik = kinematic_state->setFromIK(joint_model_group, eef_pose, attempts, timeout);
+    if (found_ik)
+    {
+        kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+    }
+    return found_ik;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "ws_drawing");
@@ -20,7 +48,7 @@ int main(int argc, char **argv)
     spinner.start();
     static const std::string LOGNAME = "ws_drawing";
 
-    // Moveit setup
+    // Get the input arguments
     std::string planning_group;
     if (nh.getParam("robot", planning_group))
     {
@@ -31,37 +59,88 @@ int main(int argc, char **argv)
         ROS_ERROR_STREAM_NAMED(LOGNAME, "No planning group specified");
         return 1;
     }
+    if (planning_group.compare("rrr") == 0)
+    {
+        ROS_WARN_STREAM_NAMED(LOGNAME, "Planning group rrr is not supported. Due to the 2D workspace. This ws_drawing is designed for the 3D workspace.");
+        return 1;
+    }
+
+    // Moveit setup
     moveit::planning_interface::MoveGroupInterface move_group(planning_group);
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
     robot_state::RobotStatePtr kinematic_state(move_group.getCurrentState());
     const robot_state::JointModelGroup *joint_model_group = kinematic_state->getJointModelGroup(planning_group);
 
-    // Zero pose via FK
-    std::vector<double> zero_joint_values(joint_model_group->getVariableCount(), 0.0);
-    kinematic_state->setJointGroupPositions(joint_model_group, zero_joint_values);
+
+    // Set a rosParam for the KDL Kinematics Plugin
+    const std::string position_only_ik_param_name =
+        "/robot_description_kinematics/" + planning_group + "/position_only_ik";
+    nh.setParam(position_only_ik_param_name, true);
+    ROS_INFO("Waiting for the param to be set");
+    while (true)
+    {
+        bool position_only = false;
+        nh.getParam(position_only_ik_param_name, position_only);
+        if (position_only) { break; }
+        else { ros::Duration(0.1).sleep(); }
+    }
+    ROS_INFO("Param setting complete!");
+
+    // Print some info
+    ROS_INFO_STREAM("      Joint tolerance: " << move_group.getGoalJointTolerance());
+    ROS_INFO_STREAM("   Position tolerance: " << move_group.getGoalPositionTolerance());
+    ROS_INFO_STREAM("Orientation tolerance: " << move_group.getGoalOrientationTolerance());
+    ROS_INFO_STREAM("      Reference frame: " << move_group.getPlanningFrame());
+    ROS_INFO_STREAM("    End effector link: " << move_group.getEndEffectorLink());
+
+    // Placeholder for the IK solution
+    std::vector<double> joint_values(joint_model_group->getVariableCount(), 0.0);
+
+    // Zero pose for the initial pose
     const std::string &eef_link = move_group.getEndEffectorLink();
-    Eigen::Affine3d eef_transformation = kinematic_state->getGlobalLinkTransform(eef_link);
-    geometry_msgs::Pose eef_pose = Eigen::toMsg(eef_transformation);
-    ROS_INFO_STREAM("Zero pose: " << eef_pose);
+    geometry_msgs::Pose zero_pose;
+    calcFK(joint_values, kinematic_state, joint_model_group, eef_link, zero_pose);
+    ROS_INFO_STREAM("Initial eef pose (zero_pose): " << zero_pose);
 
-    // Planning
-    double max_sec = move_group.getPlanningTime();
-    ROS_INFO_STREAM_NAMED(LOGNAME, "Planning time: " << max_sec);
+    /**
+     * Octree + Marching squares
+     */
+    
 
-    // move_group.setPositionTarget(eef_pose.position.x, eef_pose.position.y, eef_pose.position.z);
-    // move_group.setGoalOrientationTolerance(M_PI * 2.0);
 
+
+
+    const unsigned int attempts = 10;
+    const double timeout = 0.1;
+    bool found_ik = kinematic_state->setFromIK(joint_model_group, eef_pose, attempts, timeout);
+    if (found_ik)
+    {
+        kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+        for (std::size_t j = 0; j < joint_values.size(); ++j)
+        {
+            ROS_INFO("(IK solution) Joint %d: %f", j, joint_values[j]);
+        }
+    }
+    else
+    {
+        ROS_WARN("Did not find IK solution");
+    }
+
+    // Motion planning
+    move_group.setJointValueTarget(joint_values);
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
     bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
     if (success)
     {
+        // Result
         ROS_INFO("Planning successful");
+        ROS_INFO("Planning time: %.4f sec", my_plan.planning_time_);
         move_group.execute(my_plan);
+        ROS_INFO("Moving successful");
     }
     else
     {
-        ROS_ERROR("Planning failed");
-        return 1;
+        ROS_WARN("Planning failed");
     }
 
     ros::waitForShutdown();
