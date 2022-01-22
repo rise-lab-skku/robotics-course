eea70d0c793ae2bc0aa9fec8716a9600e3b8c0dd
Hong-ryul Jung <jung.hr.1206@gmail.com>
2022-01-22 19:04:27 +0900

    wip

--- calibration_demo/src/gyroscope.cpp
+++ calibration_demo/src/gyroscope.cpp
@@ -1,16 +1,15 @@
-#include <limits>
-#include <fstream>
-#include <yaml-cpp/yaml.h>
 #include <ros/ros.h>
-#include <ros/package.h>
-#include <sensor_msgs/MagneticField.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/Quaternion.h>
 
 // Global variables to make this code easier
+geometry_msgs::Quaternion quat;
+quat.x = 0.0;
+quat.y = 0.0;
+quat.z = 0.0;
+quat.w = 1.0;
 
-double x_min = std::numeric_limits<double>::max(), y_min = x_min, z_min = x_min;
-double x_max = std::numeric_limits<double>::max() * -1.0, y_max = x_max, z_max = x_max;
-
-void magCallback(const sensor_msgs::MagneticField::ConstPtr& msg)
+void imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
     ROS_INFO_STREAM("Mag data received."
                     << " x: " << msg->magnetic_field.x
@@ -32,47 +31,12 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     ros::AsyncSpinner spinner(1);  // Use 1 threads
     spinner.start();
-    ROS_INFO_STREAM("Node started. Waiting for mag data.");
+    ROS_INFO_STREAM("Node started. Waiting for IMU data(orientation).");
 
     // Subscribe to magnetometer data
-    ros::Subscriber mag_sub = nh.subscribe("/mag_raw", 128, &magCallback);
+    ros::Subscriber mag_sub = nh.subscribe("/imu_raw", 128, &imuCallback);
 
-    // Manually exit with Ctrl-C after `rosbag play` is complete.
+    // Manually exit with Ctrl-C.
     ros::waitForShutdown();
-    std::cout << "==================" << std::endl;
-    std::cout << "ROS was shut down." << std::endl;
-
-    // Save calibration data to file
-    // std::string package_path = ros::package::getPath("calibration_demo");
-    // std::string file_path = package_path + "/config/magnetometer_calibration.yaml";
-    // std::ofstream calibration_file;
-    // calibration_file.open(file_path);
-
-    // YAML::Emitter Tutorial
-    // https://github.com/jbeder/yaml-cpp/wiki/How-To-Emit-YAML
-    // YAML::Emitter out;
-    // out.SetIndent(2);
-    // out << YAML::BeginMap;
-    // out << YAML::Key << "x";
-    // out << YAML::Value << YAML::BeginMap
-    //     << YAML::Key << "min" << YAML::Value << x_min
-    //     << YAML::Key << "max" << YAML::Value << x_max
-    //     << YAML::EndMap;
-    // out << YAML::Key << "y";
-    // out << YAML::Value << YAML::BeginMap
-    //     << YAML::Key << "min" << YAML::Value << y_min
-    //     << YAML::Key << "max" << YAML::Value << y_max
-    //     << YAML::EndMap;
-    // out << YAML::Key << "z";
-    // out << YAML::Value << YAML::BeginMap
-    //     << YAML::Key << "min" << YAML::Value << z_min
-    //     << YAML::Key << "max" << YAML::Value << z_max
-    //     << YAML::EndMap;
-    // out << YAML::EndMap;
-
-    // calibration_file.write(out.c_str(), out.size());
-    // calibration_file.close();
-    // std::cout << "Calibration file saved at:" << std::endl;
-    // std::cout << "[ " << file_path << " ]" << std::endl;
     return 0;
 }
